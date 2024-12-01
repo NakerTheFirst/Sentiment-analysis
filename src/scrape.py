97a8d142e105822b101779144087ae39f50b247c
Krzysztof <kcholysk@gmail.com>
2024-12-01 20:48:22 +0100

    Add scraping from multiple URLs

--- src/scrape.py
+++ src/scrape.py
@@ -1,12 +1,28 @@
+import json
 import pickle
 
-from config import client, run_input
+from config import client, cookies_json_str, get_run_input, urls
 
-# Start scraping and wait for it to finish
-run = client.actor("kfiWbq3boy3dWKbiL").call(run_input=run_input, timeout_secs=10)
+actor_runs_num = len(urls)
+cookies = []
+inputs = []
+runs = []
+pages = []
 
-# Get and save raw data to file via pickle
-list_page = client.dataset(run["defaultDatasetId"]).list_items(clean=True)
+for unparsed_cookie in cookies_json_str:
+    cookie_parsed = json.loads(unparsed_cookie)
+    cookies.append(cookie_parsed)
 
-with open(r"data/raw/data_raw.bin", "wb") as filehandler:
-    pickle.dump(list_page.items, filehandler)
+for x in range(actor_runs_num):
+    inputs.append(get_run_input(urls[x], cookies[x]))
+    
+# Scrape the data 
+for x in range(actor_runs_num):
+    run = client.actor("kfiWbq3boy3dWKbiL").call(run_input=inputs[x], timeout_secs=15)
+    pages.append(client.dataset(run["defaultDatasetId"]).list_items(clean=True)) # type: ignore
+
+# Save the data
+for x in range(actor_runs_num):
+    with open(rf"data/raw/data_raw{x+1}.bin", "wb") as filehandler:
+        pickle.dump(pages[x].items, filehandler)
+        
