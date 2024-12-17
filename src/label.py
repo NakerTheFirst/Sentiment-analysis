ebfa95035380c07fe41f0eb2a194b7d1b1dd1d5d
Krzysztof <kcholysk@gmail.com>
2024-12-17 20:29:55 +0100

    Add writing to separate file for labelled data

--- src/label.py
+++ src/label.py
@@ -15,17 +15,21 @@ for index, row in in_to_label.iterrows():
         continue
 
     # Print the text of the current row
-    print("--------------------------------------------------------------")
+    print("\n\n\n\n\n\n------------------------------------------------------")
     print(f"Text to label ({index + 1}/{len(in_to_label)}): \n{row['text']}")
 
     # Ask the user to provide a label
     sentiment = input("\nProvide sentiment (1=positive, 2=neutral, 3=negative): ")
+    print("------------------------------------------------------")
 
     # Assign the sentiment value
     in_to_label.at[index, "sentiment"] = int(sentiment)
 
     # Increment the labeled counter
     labeled_count += 1
+    
+    with open(r"data/interim/labelled.bin", "wb") as filehandler:
+       pickle.dump(in_to_label, filehandler)
 
     # Break the loop if the required number of entries is labeled
     if labeled_count >= n:
