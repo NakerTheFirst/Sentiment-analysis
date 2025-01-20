bc519213ec3fa38070261585c4626d305d55d552
Krzysztof <kcholysk@gmail.com>
2025-01-20 17:12:55 +0100

    Add properly dispalyed word clouds

--- src/plots.py
+++ src/plots.py
@@ -28,7 +28,7 @@ map_sentiment_int_to_string = {
     3: "Negative"
 }
 
-#* Plot the sentiment distribution
+#* Plot the internal data sentiment distribution
 in_labelled["sentiment"] = in_labelled["sentiment"].map(map_sentiment_int_to_string)
 hist_plot = sns.histplot(in_labelled["sentiment"], discrete=True, stat="percent")
 fig = hist_plot.get_figure()
@@ -36,6 +36,9 @@ plt.title('Sentiment distribution')
 plt.ylabel('Percent')
 plt.xlabel('Sentiment')
 plt.show()
+
+#* Plot TL data sentiment distribution
+# Placeholder
 
 # Save the plot
 # fig.savefig("reports/figures/sentiment_hist.png") # type: ignore
@@ -45,16 +48,22 @@ plt.close()
 # Combine all text into one string
 text = " ".join(in_labelled["text"])
 
-# Create the word cloud
+# Create and generate the wordcloud
 wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
 
-# Set the display options
+# Create a new figure with a specific size
 plt.figure(figsize=(10, 5))
+
+# Display the wordcloud
 plt.imshow(wordcloud, interpolation='bilinear')
-plt.title('Word cloud of data')
+plt.title('Word cloud of LiknedIn data')
 plt.axis('off')
+
+# Save the figure before showing it
+# plt.savefig("reports/figures/wordcloud.png", bbox_inches='tight', dpi=300)
+
+# Show the plot
 plt.show()
 
-# Save the word cloud 
-# plt.savefig("reports/figures/wordcloud.png")
+# Close the figure
 plt.close()
